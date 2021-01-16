0e21641f98dbf31bcd565205fcdf10d41f0b54ac
julian-patterson <77037728+julian-patterson@users.noreply.github.com>
2021-01-15 23:02:23 -0500

    Add files via upload

--- color.py
+++ color.py
@@ -2,6 +2,8 @@ import time
 from rpi_ws281x import PixelStrip, Color
 import argparse
 
+strip.begin()
+
 #LED strip configuration
 LED_COUNT = 80
 LED_PIN = 18
@@ -22,11 +24,16 @@ def colorWipe(strip, color, wait_ms=50):
         time.sleep(wait_ms / 1000.0)
 
 #Color-variable assignment
-blue = colorWipe(strip, Color(0, 255, 0))
-red = colorWipe(strip, Color(255, 0, 0))
-purple = colorWipe(strip, Color(128, 128, 0))
-pink = colorWipe(strip, Color(255, 203, 192))
-yellow = colorWipe(strip, Color(255, 0, 255))
+def blue():
+    colorWipe(strip, Color(0, 255, 0))
+def red():
+    colorWipe(strip, Color(255, 0, 0))
+def purple():
+    colorWipe(strip, Color(128, 128, 0))
+def pink():
+    colorWipe(strip, Color(255, 203, 192))
+def yellow():
+    colorWipe(strip, Color(255, 0, 255))
 
 #Main Code
 if __name__ == '__main__':
@@ -40,11 +47,23 @@ if __name__ == '__main__':
     print('Press Ctrl-C to quit.')
     if not args.clear:
         print('Use -c arguement to clear LEDs on exit')
-    input = ('What color would you like: ')
+    userinput = input('What color would you like: ')
 
     try:
         while True:
-            colorWipe(strip, Color(248, 0, 100))
+            if userinput == 'blue':
+                yellow()
+            if userinput == 'red':
+                red()
+            if userinput == 'purple':
+                purple()
+            if userinput == 'pink':
+                pink()
+            if userinput == 'yellow':
+                yellow()
+            else:
+                print = ('Please enter a valid color')
+                userinput = input('What color would you like: ')
     except KeyboardInterrupt:
         if args.clear:
             colorWipe(strip, Color(0,0,0), 10)    
