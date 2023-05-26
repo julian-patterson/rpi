b57779aaaa14d257475ed039265ff36c017fcd69
julian-patterson <julianedwardpatterson@gmail.com>
2023-05-26 15:20:12 -0400

    finalized colorChange

--- colorChange.py
+++ colorChange.py
@@ -1,7 +1,7 @@
 import argparse
 import time
 
-import rpi_ws281x
+from rpi_ws281x import Color, PixelStrip
 
 LED_COUNT = 100
 LED_PIN = 18
@@ -11,73 +11,55 @@ LED_BRIGHTNESS = 255
 LED_INVERT = False
 LED_CHANNEL = 0
 
-strip = rpi_ws281x.Adafruit_NeoPixel(
-    LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_BRIGHTNESS, LED_INVERT, LED_CHANNEL)
-
-pixels = strip.numPixels()
-
-
-def color_change():
-    while True:
-        for red in range(0, 256):
-            strip.setPixelColorRGB(pixels, red, green, blue)
-            strip.show()
-            time.sleep(0.5)
-
-        for green in range(0, 256):
-            strip.setPixelColorRGB(pixels, red, green, blue)
-            strip.show()
-            time.sleep(0.5)
-
-        for red in range(255, 0, -1):
-            strip.setPixelColorRGB(pixels, red, green, blue)
-            strip.show()
-            time.sleep(0.5)
-
-        for blue in range(0, 256):
-            strip.setPixelColorRGB(pixels, red, green, blue)
-            strip.show()
-            time.sleep(0.5)
-
-        for green in range(255, 0, -1):
-            strip.setPixelColorRGB(pixels, red, green, blue)
-            strip.show()
-            time.sleep(0.5)
-
-        for red in range(0, 256):
-            strip.setPixelColorRGB(pixels, red, green, blue)
-            strip.show()
-            time.sleep(0.5)
-
-        for blue in range(255, 0, -1):
-            strip.setPixelColorRGB(pixels, red, green, blue)
-            strip.show()
-            time.sleep(0.5)
-
-
-def lights():
-    if __name__ == '__main__':
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-c', '--clear', action='store_true',
-                            help='clear the display on exit')
-        args = parser.parse_args()
-
-        strip = rpi_ws281x.Adafruit_NeoPixel(
-            LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-        strip.begin()
-
-        print('Press Ctrl-C to quit.')
-        if not args.clear:
-            print('Use -c arguement to clear LEDs on exit')
-
-        try:
-            while True:
-                color_change()
-
-        except KeyboardInterrupt:
-            if args.clear:
-                strip.setPixelColorRGB(pixels, 0, 0, 0)
-
 
 def color(red, green, blue, white=0):
     return (white << 24) | (red << 16) | (green << 8) | blue
+
+
+def colorWipe(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true',
+                        help='clear the display on exit')
+    args = parser.parse_args()
+
+    strip = PixelStrip(
+        LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use -c arguement to clear LEDs on exit')
+
+    try:
+        while True:
+            pixels = strip.numPixels()
+            colorWipe(strip, Color(255, 0, 0))
+            red = 255
+            blue = 0
+            for green in range(0, 256, 5):
+                colorWipe(strip, Color(red, green, blue))
+
+            for red in range(255, 0, -5):
+                colorWipe(strip, Color(red, green, blue))
+
+            for blue in range(0, 256, 5):
+                colorWipe(strip, Color(red, green, blue))
+
+            for green in range(255, 0, -5):
+                colorWipe(strip, Color(red, green, blue))
+
+            for red in range(0, 256, 5):
+                colorWipe(strip, Color(red, green, blue))
+
+            for blue in range(255, 0, -5):
+                colorWipe(strip, Color(red, green, blue))
+
+    except KeyboardInterrupt:
+        if args.clear:
+            strip.setPixelColorRGB(pixels, 0, 0, 0)
